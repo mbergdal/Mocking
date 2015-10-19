91c38bf21bee972fb910e5c83d108311db88f655
mbergdal@gmail.com <Vinter12>
2015-10-20 01:03:03 +0200

    Removed some things

--- test/TrackingServiceTests.java
+++ test/TrackingServiceTests.java
@@ -9,11 +9,6 @@ public class TrackingServiceTests {
 
     private TrackingService service;
 
-    @Before
-    public void setUp()
-    {
-        service = new TrackingService(new NotifierStub());
-    }
 
     @Test
     public void WhenGoalIsMetHistoryIsUpdated() throws InvalidGoalException
@@ -24,36 +19,16 @@ public class TrackingServiceTests {
         assertEquals("sent:goal met", result.getOperation());
     }
 
-    @Test
-    public void WhenGoalIsMetHistoryIsUpdated_withMockito() throws InvalidGoalException
-    {
-        Notifier mockitoNotifier = mock(Notifier.class);
-        service = new TrackingService(mockitoNotifier);
-        when(mockitoNotifier.send("goal met")).thenReturn(true);
-        service.setGoal(5);
-
-        service.addProtein(6);
-        HistoryItem result = service.getHistory().get(1);
-
-        assertEquals("sent:goal met", result.getOperation());
-        verify(mockitoNotifier, times(1)).send("goal met");
-    }
-
-    @Test
+    //@Test
     //This is an integration test
-    public void WhenGoalIsMetMailIsSent() throws InvalidGoalException
-    {
-        service = new TrackingService(new Emailer());
-        service.setGoal(5);
-        service.addProtein(6);
-        HistoryItem result = service.getHistory().get(1);
-        assertEquals("sent:goal met", result.getOperation());
-    }
+//    public void WhenGoalIsMetMailIsSent() throws InvalidGoalException
+//    {
+//        service = new TrackingService(new Emailer());
+//        service.setGoal(5);
+//        service.addProtein(6);
+//        HistoryItem result = service.getHistory().get(1);
+//        assertEquals("sent:goal met", result.getOperation());
+//    }
 
-    private class NotifierStub implements Notifier {
-        @Override
-        public boolean send(String message) {
-            return true;
-        }
-    }
+
 }
